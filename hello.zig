3bc220c7d72da4fd10ae81dc74f9cf6f3028c3cf
Guillaume <guillaume.thouvenin@vates.tech>
2025-04-24 19:00:37 +0200

    [hello] print into a file

--- hello.zig
+++ hello.zig
@@ -1,5 +1,13 @@
 const std = @import("std");
 
-pub fn main() void {
-    std.log.info("Hello, Sailor", .{});
+const fname = "output.txt";
+
+pub fn main() !void {
+    // Let's write Hello, Sailor into a file
+    std.log.info("writing {s}", .{fname});
+
+    const f = try std.fs.Dir.createFile(std.fs.cwd(), fname, .{ .truncate = true });
+    defer f.close();
+
+    try f.writeAll("Hello, Sailor!");
 }
