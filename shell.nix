c9e19e277c49d18e75647a903208a32d86071bdf
Guillaume <gthvn1@gmail.com>
2025-04-25 22:06:21 +0200

    [nix] add nix shell for rust

---
+++ shell.nix
@@ -0,0 +1,12 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {} }:
+
+pkgs.mkShell {
+  packages = with pkgs; [
+    cargo
+    clippy
+    rust-analyzer
+    rustc
+    rustfmt
+  ];
+}
+
