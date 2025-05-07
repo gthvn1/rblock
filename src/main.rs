08d685c83dde347f683d5bf6fe9987424716408c
Guillaume <guillaume.thouvenin@vates.tech>
2025-05-07 20:54:21 +0200

    [main] allow to pass a qcow file as parameter

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
-use std::fs;
+use std::{env, fs};
 
 use rblock::qcow2::Qcow2;
 use rblock::server::start_server;
-
-const IFNAME: &str = "samples/input.txt";
-const OFNAME: &str = "samples/output.txt";
-const QCOWFNAME: &str = "samples/disk.qcow2";
 
 fn rot13(v: Vec<u8>) -> Vec<u8> {
     let mut rotv: Vec<u8> = Vec::new();
@@ -22,20 +18,34 @@ fn rot13(v: Vec<u8>) -> Vec<u8> {
     rotv
 }
 
-fn testing_qcow2() {
+fn testing_qcow2(fname: &str) {
     print!("Testing QCOW2: ");
-    let qcow = Qcow2::new(QCOWFNAME).expect("Failed to read qcow file");
+    let qcow = Qcow2::new(fname).expect("Failed to read qcow file");
     println!("Detected qcow version {}", qcow.version());
 }
 
 fn testing_rot13() {
+    const IFNAME: &str = "samples/input.txt";
+    const OFNAME: &str = "samples/output.txt";
     println!("Testing ROT13: {} -> {}", IFNAME, OFNAME);
     let buf = fs::read(IFNAME).unwrap_or_else(|_| panic!("Failed to read {}", IFNAME));
     fs::write(OFNAME, rot13(buf)).unwrap_or_else(|_| panic!("Failed to write {}", OFNAME));
 }
 
 fn main() {
-    testing_qcow2();
+    const QCOWFNAME: &str = "samples/disk.qcow2";
+    let mut arguments = env::args();
+
+    // Skip the first argument that is the name of program
+    let _progname = arguments.next();
+
+    let fname = match arguments.next() {
+        None => QCOWFNAME.to_string(),
+        Some(f) => f,
+    };
+
+    println!("qcow file: {}", fname);
+    testing_qcow2(&fname);
     testing_rot13();
     start_server();
 }
